d230f4e1da20462b135c423a25bbb933424e791f
Neeraj Rekwar <88387699+neerajrekwar@users.noreply.github.com>
2024-01-17 00:11:34 +0530

    Create crousal.js

---
+++ crousal.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.carousel').slick({
+        autoplay: true,
+        autoplaySpeed: 2000, // Adjust the speed as needed
+        dots: true, // Show navigation dots
+        arrows: true // Show navigation arrows
+        // Add more configuration options based on your requirements
+    });
+});
